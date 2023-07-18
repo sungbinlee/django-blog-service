ac15a73cc5aa3f965248bd821ec9864ed8a7535a
sungbinlee <sunglee@addu.edu.ph>
2023-07-18 16:30:57 +0900

    [refactor] 파일 구조 최적화

--- blogservice/blogservice/urls.py
+++ blogservice/blogservice/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog.views import MainView
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', MainView.as_view(), name='main'),
+    path('', views.IndexMain.as_view()),
     path('user/', include('user.urls')),
 ]
