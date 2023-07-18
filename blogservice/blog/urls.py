2af98c2ca138fead1dbc7b8b9c02decb0bc2f05f
sungbinlee <sunglee@addu.edu.ph>
2023-07-18 17:57:20 +0900

    [feat] 게시글 리스트 보기 추가

--- blogservice/blog/urls.py
+++ blogservice/blog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import CreatePostView
+from .views import CreatePostView, PostListView
 
 app_name = 'blog'
 
 urlpatterns = [
+    # 글목록 조회
+    path('', PostListView.as_view(), name='post_list'),
+    # 글 생성
     path('post/create/', CreatePostView.as_view(), name='create_post'),
+    
 ]
--- blogservice/blog/views.py
+++ blogservice/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from .forms import PostForm
+from .models import Post
+
 
 class CreatePostView(View):
     def get(self, request):
@@ -22,3 +24,12 @@ class CreatePostView(View):
                 'form': form
             }
             return render(request, 'blog/create_post.html', context)
+
+class PostListView(View):
+    def get(self, request):
+        # 글 목록 조회
+        posts = Post.objects.all()
+        context = {
+            'posts': posts
+        }
+        return render(request, 'blog/post_list.html', context)
