3eb7c175d34d778fc4ee74b928f1c9433cd44e01
sungbinlee <sunglee@addu.edu.ph>
2023-07-18 21:50:20 +0900

    [feat] 글조회, 수정, 삭제 구현

--- blogservice/blog/urls.py
+++ blogservice/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import CreatePostView, PostListView
+from .views import CreatePostView, PostListView, PostDetailView, PostUpdateView, PostDeleteView
+
 
 app_name = 'blog'
 
@@ -8,5 +9,8 @@ urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
     # 글 생성
     path('post/create/', CreatePostView.as_view(), name='post_create'),
+    path('<int:post_id>/', PostDetailView.as_view(), name='post_detail'),
+    path('<int:post_id>/update/', PostUpdateView.as_view(), name='post_update'),
+    path('<int:post_id>/delete/', PostDeleteView.as_view(), name='post_delete'),
     
 ]
--- blogservice/blog/views.py
+++ blogservice/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from .forms import PostForm
 from .models import Post
@@ -25,6 +25,7 @@ class CreatePostView(View):
             }
             return render(request, 'blog/post_create.html', context)
 
+
 class PostListView(View):
     def get(self, request):
         # 글 목록 조회
@@ -33,3 +34,55 @@ class PostListView(View):
             'posts': posts
         }
         return render(request, 'blog/post_list.html', context)
+
+
+class PostDetailView(View):
+    def get(self, request, post_id):
+        # 글 상세 조회
+        post = get_object_or_404(Post, id=post_id)
+        context = {
+            'post': post
+        }
+        return render(request, 'blog/post_detail.html', context)
+
+
+class PostUpdateView(View):
+    def get(self, request, post_id):
+        # 글 수정 폼 보여주기
+        post = get_object_or_404(Post, id=post_id)
+        form = PostForm(instance=post)
+        context = {
+            'post': post,
+            'form': form
+        }
+        return render(request, 'blog/post_update.html', context)
+
+    def post(self, request, post_id):
+        # 글 수정 처리
+        post = get_object_or_404(Post, id=post_id)
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('blog:post_detail', post_id=post.id)
+        else:
+            context = {
+                'post': post,
+                'form': form
+            }
+            return render(request, 'blog/post_update.html', context)
+
+
+class PostDeleteView(View):
+    def get(self, request, post_id):
+        # 글 삭제 확인 페이지 보여주기
+        post = get_object_or_404(Post, id=post_id)
+        context = {
+            'post': post
+        }
+        return render(request, 'blog/post_delete.html', context)
+
+    def post(self, request, post_id):
+        # 글 삭제 처리
+        post = get_object_or_404(Post, id=post_id)
+        post.delete()
+        return redirect('blog:post_list')
