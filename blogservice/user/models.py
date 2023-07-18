6046940c9cf0aae84fb4b53961822a6113221975
sungbinlee <sunglee@addu.edu.ph>
2023-07-18 09:41:26 +0900

    [feat] User Model 정의

--- blogservice/user/models.py
+++ blogservice/user/models.py
@@ -2,8 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    nickname = models.CharField(max_length=255)
-    is_email_verified = models.BooleanField(default=False)
     social_provider = models.CharField(max_length=255, blank=True, null=True)
     social_id = models.CharField(max_length=255, blank=True, null=True)
     profile_picture = models.CharField(max_length=255, blank=True, null=True)
