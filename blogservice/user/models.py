062339619bbd3bf5028cc7cc755b29e3eb732f8d
sungbinlee <sunglee@addu.edu.ph>
2023-07-18 10:02:45 +0900

    [fix] models class 띄어쓰기 수정

--- blogservice/user/models.py
+++ blogservice/user/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     social_provider = models.CharField(max_length=255, blank=True, null=True)
     social_id = models.CharField(max_length=255, blank=True, null=True)
