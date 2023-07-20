90e15c1a27c8f3a645cbe7607ffca065f33ef99b
sungbinlee <sunglee@addu.edu.ph>
2023-07-20 11:39:52 +0900

    [design] 부트스트랩 적용 및 전체 페이지 디자인 수정

--- blogservice/blog/forms.py
+++ blogservice/blog/forms.py
@@ -2,34 +2,34 @@ from django import forms
 from .models import Post, Comment, Image
 
 class PostForm(forms.ModelForm):
-
     tags = forms.CharField(
         required=False,
         widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '태그 입력 (쉼표로 구분)'}),
     )
 
-
-    def clean_tags(self):
-        tags = self.cleaned_data.get('tags')
-        if tags:
-            tags = [tag.strip() for tag in tags.split(',') if tag.strip()]
-        return tags
-
-
     class Meta:
         model = Post
         fields = ['title', 'content', 'category', 'tags']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 6}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+        }
 
 
 class CommentForm(forms.ModelForm):
-    content = forms.CharField(widget=forms.Textarea(attrs={'rows': 4}))
-
     class Meta:
         model = Comment
         fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+        }
 
 
 class ImageForm(forms.ModelForm):
     class Meta:
         model = Image
         fields = ['file_path']
+        widgets = {
+            'file_path': forms.FileInput(attrs={'class': 'form-control'}),
+        }
--- blogservice/user/forms.py
+++ blogservice/user/forms.py
@@ -12,11 +12,22 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['email']
-        # fields = UserCreationForm.Meta.fields + ('email',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Email'})
+        self.fields['password1'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Password'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Confirm Password'})
+
 
 
 class LoginForm(AuthenticationForm):
-     
-     class Meta:
+    
+    class Meta:
         model = User
         fields = ['email', 'password']
+
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'form-control', 'placeholder': '이메일'})
+        self.fields['password'].widget.attrs.update({'class': 'form-control', 'placeholder': '비밀번호'})
