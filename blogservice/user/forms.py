874407af34b21868abfeec891dd18466a0f2fc1b
sungbinlee <sunglee@addu.edu.ph>
2023-07-20 23:52:50 +0900

    [feat] 비밀번호 변경기능 추가

--- blogservice/user/forms.py
+++ blogservice/user/forms.py
@@ -63,6 +63,20 @@ class LoginForm(AuthenticationForm):
 
 
 class UserUpdateForm(forms.ModelForm):
+    password1 = forms.CharField(
+        label="새 비밀번호",
+        strip=False,
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '비밀번호를 입력하세요'}),
+        required=False,
+        help_text=password_validators_help_text_html(),
+    )
+    password2 = forms.CharField(
+        label="새 비밀번호 확인",
+        strip=False,
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '비밀번호 확인'}),
+        required=False,
+    )
+
     class Meta:
         model = User
         fields = ['name', 'profile_picture', 'introduction']
@@ -70,3 +84,31 @@ class UserUpdateForm(forms.ModelForm):
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'introduction': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
         }
+
+    def clean_password1(self):
+        password1 = self.cleaned_data.get('password1')
+        if password1:
+            try:
+                validate_password(password1, self.instance)
+            except ValidationError as error:
+                self.add_error('password1', error)
+        return password1
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get('password1')
+        password2 = cleaned_data.get('password2')
+
+        if password1 and password2 and password1 != password2:
+            self.add_error('password2', "비밀번호가 일치하지 않습니다.")
+
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        password = self.cleaned_data.get('password1')
+        if password:
+            user.set_password(password)
+        if commit:
+            user.save()
+        return user
