625efb8df1a7cbad88daeab322f553780686de22
sungbinlee <sunglee@addu.edu.ph>
2023-07-20 17:21:04 +0900

    [design] 글 조회 디자인 추가

--- blogservice/blog/views.py
+++ blogservice/blog/views.py
@@ -50,7 +50,7 @@ class CreatePostView(LoginRequiredMixin, View):
 class PostListView(View):
     def get(self, request):
         # 글 목록 조회
-        posts = Post.objects.all()
+        posts = Post.objects.all().order_by('-created_at')
         context = {
             'posts': posts
         }
