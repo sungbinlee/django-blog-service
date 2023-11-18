1d0d6db6d1f201b47bd96c871c2c745e3acdbe36
Sungbin Lee <52542229+sungbinlee@users.noreply.github.com>
2023-11-18 18:47:01 +0900

    [test] 회원가입 & 이메일 발송 테스트케이스 작성

--- blogservice/user/tests.py
+++ blogservice/user/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+class UserRegistrationTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.register_url = reverse('user:register')
+        self.user_data = {
+            'email': 'testuser@example.com',
+            'name': 'test',
+            'password1': 'testpassword123',
+            'password2': 'testpassword123',
+        }
+
+    def test_user_registration_view_get(self):
+        response = self.client.get(self.register_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'user/register.html')
+
+    def test_user_registration_view_post(self):
+        response = self.client.post(self.register_url, self.user_data, follow=True)
+        self.assertRedirects(response, reverse('user:verification_sent'))
