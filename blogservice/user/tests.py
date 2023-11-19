a99787e50a7dc0de3c6465c00788178ce866a3cd
Sungbin Lee <52542229+sungbinlee@users.noreply.github.com>
2023-11-19 17:24:24 +0900

    test: 로그인 테스트 케이스 추가

--- blogservice/user/tests.py
+++ blogservice/user/tests.py
@@ -20,3 +20,31 @@ class UserRegistrationTestCase(TestCase):
     def test_user_registration_view_post(self):
         response = self.client.post(self.register_url, self.user_data, follow=True)
         self.assertRedirects(response, reverse('user:verification_sent'))
+
+
+class LoginTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.login_url = reverse('user:login')
+        self.user = User.objects.create_user(email='testuser@example.com', password='testpassword123', is_staff=False, is_superuser=False,is_active=True)
+
+    def test_login_view_get(self):
+        response = self.client.get(self.login_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'user/login.html')
+
+    def test_login_view_post_valid_credentials(self):
+        login_data = {
+            'email': 'testuser@example.com',
+            'password': 'testpassword123',
+        }
+        response = self.client.post(self.login_url, login_data, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_view_post_invalid_credentials(self):
+        login_data = {
+            'email': 'testuser@example.com',
+            'password': 'wrongpassword',
+        }
+        response = self.client.post(self.login_url, login_data)
+        self.assertEqual(response.status_code, 200)
